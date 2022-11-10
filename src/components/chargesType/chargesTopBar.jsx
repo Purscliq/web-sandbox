3df8a332bcf53f63d0181e9fdc630b52dd71b089
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2022-11-10 13:34:20 +0100

    nested route fully implemented for different charge type

--- src/components/chargesType/chargesTopBar.jsx
+++ src/components/chargesType/chargesTopBar.jsx
@@ -1,7 +1,9 @@
+import React, { useState } from "react";
 import Logo from "../../assets/images/purseblue.png";
 import { IoMdNotifications } from "react-icons/io";
 
 export default function ChargesTopBar(props) {
+  const [pageName] = useState("charges")
   return (
     <div>
       <div className="w-screen flex-shrink-0 flex h-16 bg-blackPearl shadow fixed top-0 left-0 z-10">
@@ -10,7 +12,7 @@ export default function ChargesTopBar(props) {
             <div className="flex items-center h-16 flex-shrink-0 px-4">
               <img className="h-8 w-auto" src={Logo} alt="Workflow" />
             </div>
-            <p className="text-primary text-2xl mx-7 my-4">{props.pageName}</p>
+            <p className="text-primary text-2xl mx-7 my-4">{pageName}</p>
           </div>
 
           <div className="ml-4 flex items-center md:ml-6">
--- src/pages/charges.jsx
+++ src/pages/charges.jsx
@@ -1,77 +1,30 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ChargesTypeWrapper from "../components/chargesType/chargeWrapper";
 import ChargesTopBar from "../components/chargesType/chargesTopBar";
 import { Sidebar } from "../components";
 import {Outlet} from "react-router-dom"
 
-export class Charges extends Component {
-  state = {
-    pageName: "Charges",
-    chargesType: [
+export const Charges = () => {
+  const [chargesType] = useState(
+    [
       { type: "Virtual Card Charges", id: 1, name: "virtualcard" },
       { type: "Physical Charges", id: 2, name: "physicalcharges" },
       { type: "Bill Payment", id: 3, name: "billpayment" },
       { type: "Electricity Charges", id: 4, name: "electricitycharges" },
       { type: "Gift Card Charges", id: 5, name: "giftcards" },
       { type: "General Transaction Fee", id: 6, name: "generaltransaction" },
-    ],
-    virtualCardCharges: [
-      
-    ],
-    physicalCharges: [
-      
-    ],
-    billPayment: [
-      
-    ],
-    electricityCharges: [
-      {
-        id: 10,
-        email: "usermail@gmail.com",
-        phone: +2341045454545,
-        creationFee: 100,
-        transactFee: 10100,
-        monthlyFee: 10101000,
-        purchaseFee: 1000,
-      },
-      {
-        id: 11,
-        email: "usermail@gmail.com",
-        phone: +2341145454545,
-        creationFee: 110,
-        transactFee: 11110,
-        monthlyFee: 11111100,
-        purchaseFee: 1100,
-      },
-      {
-        id: 12,
-        email: "usermail@gmail.com",
-        phone: +2341245454545,
-        creationFee: 120,
-        transactFee: 12120,
-        monthlyFee: 12121200,
-        purchaseFee: 1200,
-      },
-    ],
-    giftCards: [
-      
-    ],
-    generalTransactionFee: [
-      
-    ],
-  };
-  render() {
+    ]
+  )
     return (
       <div className="w-full h-screen bg-gray600">
-        <ChargesTopBar pageName={this.state.pageName} />
+        <ChargesTopBar />
         <Sidebar />
         <section className="w-10/12 mx-auto mt-16 bg-gray600 relative">
           <div className="mb-4">
-            <ChargesTypeWrapper charges={this.state.chargesType} />
+            <ChargesTypeWrapper charges={chargesType} />
           </div>
           <Outlet/>
         </section>
       </div>
     );
   }
-}
